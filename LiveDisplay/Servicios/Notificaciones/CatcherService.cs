3ad23798653dd69ed15bc95c28e4a07568ca0871
Carlos Cepeda <carlosalt5126@hotmail.es>
2018-06-04 16:38:03 -0500

    Working in Sensors: Log the Accelerometer Axis values so can take note about how to awake the device: TODO: Make the device turn on after lifting it back up; Make the device turn On after being in a Pocket. Check also the proximity sensor.

--- LiveDisplay/Servicios/Notificaciones/CatcherService.cs
+++ LiveDisplay/Servicios/Notificaciones/CatcherService.cs
@@ -1,8 +1,10 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Hardware;
 using Android.Media;
 using Android.Media.Session;
 using Android.OS;
+using Android.Runtime;
 using Android.Service.Notification;
 using Android.Util;
 using LiveDisplay.Adapters;
@@ -19,10 +21,14 @@ namespace LiveDisplay.Servicios
 {
     [Service(Label = "Catcherrr", Permission = "android.permission.BIND_NOTIFICATION_LISTENER_SERVICE")]
     [IntentFilter(new[] { "android.service.notification.NotificationListenerService"})]
-    internal class Catcher : NotificationListenerService
+    internal class Catcher : NotificationListenerService, ISensorEventListener
     {
         CatcherHelper catcherHelper;
         List<StatusBarNotification> statusBarNotifications;
+        SensorManager sensorManager;
+        Sensor sensorAccelerometer;
+        ISensorListener sensorListener;
+
         
         public override IBinder OnBind(Intent intent)
         {
@@ -32,16 +38,20 @@ namespace LiveDisplay.Servicios
                 RetrieveNotificationFromStatusBar();
                 SubscribeToEvents();
                 RegisterReceivers();
+                StartWatchingDeviceMovement();
             }           
             return base.OnBind(intent);
 
         }
+
+
+
         public override void OnListenerConnected()
         {            
             SubscribeToEvents();
             RegisterReceivers();
             RetrieveNotificationFromStatusBar();
-
+            StartWatchingDeviceMovement();
         }
         public override void OnNotificationPosted(StatusBarNotification sbn)
         {
@@ -115,6 +125,52 @@ namespace LiveDisplay.Servicios
             CancelAllNotifications();
             catcherHelper.CancelAllNotifications();
         }
+        //Sensor Implementation
+        private void StartWatchingDeviceMovement()
+        {
+            sensorManager = (SensorManager)GetSystemService(SensorService);
+            sensorAccelerometer = sensorManager.GetDefaultSensor(SensorType.Accelerometer);
+            sensorManager.RegisterListener(this, sensorAccelerometer, SensorDelay.Normal);
+        }
+        public void OnAccuracyChanged(Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
+        {
+            //Nothing yet.
+        }
 
+        public void OnSensorChanged(SensorEvent e)
+        {
+            if (e.Sensor.Type == SensorType.Accelerometer)
+            {
+                Console.WriteLine("X= " + e.Values[0]);//Aceleración en el eje X- Gx;
+                Console.WriteLine("Y= " + e.Values[1]);//Aceleración en el eje Y- Gy;
+                Console.WriteLine("Z= " + e.Values[2]);//Aceleración en el eje Z- Gz;
+
+                //TODO:
+
+                //Detect phone on plain surface:
+                //Z axis must have the following value: 
+                //>10 m/s2; 
+                //Y axis must be less than 3m/s2 so, the device can be slightly tilted and still being
+                //in a Plain surface.
+                //Put a timer of 3 seconds, and if the device is still with these values,
+                //the phone is left in a plain surface.
+                //New feature? Don't awake phone on new Notification while phone is left alone
+                //To avoid Unnecesary awake if the user won't see it.
+
+                //Detect if User has grabbed the phone back up:
+                //Z axis must be less than 10 m/s2("Example: 9.5") it means that Z  axis is not being
+                //Accelerated and
+                //Y axis must be greater than 3m/s2
+
+                //The less Z axis m/s2 value is, and the more Y axis m/s2 value is, the phone more vertically is.
+
+                //Notes:
+                //X axis is not necessary as I don't need to know if the phone is being moved Horizontally.
+
+
+
+            }
+        }
+        
     }
 }
