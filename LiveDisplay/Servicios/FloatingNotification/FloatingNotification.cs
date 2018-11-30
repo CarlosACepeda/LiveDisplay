0ee8f7315fde87b49900450c497427d83c2b10f6
Carlos Cepeda <carlosalt5126@hotmail.es>
2018-11-30 14:13:18 -0500

    Working in the Floating Notification, added a service that will spawn this widget while the music is playing.

---
+++ LiveDisplay/Servicios/FloatingNotification/FloatingNotification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace LiveDisplay.Servicios.FloatingNotification
+{
+    //Will spawn a View to show the clicked notification in the list, while the music is playing.
+    class FloatingNotification : Service
+    {
+        private View floatingNotificationView;
+        private TextView floatingNotificationTitle, floatingNotificationText;
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+        public override void OnCreate()
+        {
+            
+            base.OnCreate();
+            IWindowManager windowManager = (IWindowManager)GetSystemService(WindowService);
+        }
+    }
+}
