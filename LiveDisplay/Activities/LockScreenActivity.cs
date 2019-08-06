CarlosACepeda/LiveDisplay

commit fbcdb4ee6140288a3c6390692d6de94e08c9d6cf

When the notification is posted and the music is Playing and the screen is off now it spawns the floating view with the notification that was posted, the previous behavior was to only wake up the screen. (only happened when the Music Widget is present of course) also deleting redundant code in MainActivity Fixing a tiny bug where you can't remove the first notification on the list when long clicking it.

LiveDisplay/Servicios/FloatingNotification/FloatingNotification.cs
@@ -115,6 +115,7 @@ namespace LiveDisplay.Servicios.FloatingNotification
             {
                 Awake.WakeUpScreen();
             }
+            floatingNotificationView.Visibility = ViewStates.Visible;
         }
 
         private void CatcherHelper_NotificationRemoved(object sender, EventArgs e)
@@ -124,7 +125,7 @@ namespace LiveDisplay.Servicios.FloatingNotification
 
         private void CatcherHelper_NotificationUpdated(object sender, NotificationItemClickedEventArgs e)
         {
-            if (position > -1) //Avoid out of bounds exceptions, i guess, the out of bounds vaue is -1, i guess, too.
+            if (position > -1) //Avoid out of bounds exceptions, i guess, the out of bounds value is -1.
                 using (OpenNotification openNotification = new OpenNotification(e.Position))
                 {
                     floatingNotificationAppName.Text = openNotification.AppName();
@@ -167,7 +168,7 @@ namespace LiveDisplay.Servicios.FloatingNotification
         private void NotificationAdapterViewHolder_ItemLongClicked(object sender, NotificationItemClickedEventArgs e)
         {
             position = e.Position;
-            if (position > 0) //Avoid out of bounds exceptions, i guess, the out of bounds vaue is -1, i guess, too.
+            if (position > -1) //Avoid out of bounds exceptions, i guess, the out of bounds vaue is -1, i guess, too.
                 using (OpenNotification openNotification = new OpenNotification(e.Position))
                 {
                     if (openNotification.IsRemovable())

LiveDisplay/Activities/MainActivity.cs
@@ -114,15 +114,9 @@
 
         private void IsApplicationHealthy()
         {
-            bool canDrawOverlays = true;
-            if (Build.VERSION.SdkInt > BuildVersionCodes.LollipopMr1) //In Lollipop and less this permission is granted at Install time.
-            {
-                canDrawOverlays = Checkers.ThisAppCanDrawOverlays();
-            }
-
             using (var accessestext = FindViewById<TextView>(Resource.Id.health))
             {
-                if (Checkers.IsNotificationListenerEnabled() == true && Checkers.IsThisAppADeviceAdministrator() && canDrawOverlays == true)
+                if (Checkers.IsNotificationListenerEnabled() == true && Checkers.IsThisAppADeviceAdministrator() && Checkers.ThisAppCanDrawOverlays())
                 {
                     accessestext.SetText(Resource.String.accessesstatusenabled);
                     accessestext.SetTextColor(Android.Graphics.Color.Green);

LiveDisplay/Resources/Resource.Designer.cs
@@ -2526,26 +2526,8 @@ namespace LiveDisplay
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02007c
-			public const int avd_hide_password_1 = 2130837628;
-			
-			// aapt resource value: 0x7f02007d
-			public const int avd_hide_password_2 = 2130837629;
-			
-			// aapt resource value: 0x7f02007e
-			public const int avd_hide_password_3 = 2130837630;
-			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
-			
-			// aapt resource value: 0x7f02007f
-			public const int avd_show_password_1 = 2130837631;
-			
-			// aapt resource value: 0x7f020080
-			public const int avd_show_password_2 = 2130837632;
-			
-			// aapt resource value: 0x7f020081
-			public const int avd_show_password_3 = 2130837633;
 			
 			// aapt resource value: 0x7f020055
 			public const int baseline_close_white_18 = 2130837589;

commit 8e3e0d871a266d400107fe3c62ab39a0cbdebc74

Fixing a bug introduced in commit: fbcdb4e
Fixing a really strange bug where the Music Widget despite the music was playing it still got removed despite the code was being written correctly (Only despawn the music widget if the music was paused after 5 seconds), maybe a Xamarin bug, however it was fixed by using curly braces because it seems that using one-liner if's causes that if to not be evaluated, thus causing that bug.
Shrug xd.

LiveDisplay/Activities/LockScreenActivity.cs
@@ -135,6 +135,12 @@
             LoadConfiguration();
 
             CheckNotificationListSize();
+            OnActivityStateChanged += LockScreenActivity_OnActivityStateChanged;
+        }
+
+        private void LockScreenActivity_OnActivityStateChanged(object sender, LockScreenLifecycleEventArgs e)
+        {
+            currentActivityState = e.State;
         }
 
         private void Fadeoutanimation_AnimationEnd(object sender, Animation.AnimationEndEventArgs e)
@@ -161,38 +167,42 @@
 
         private void CheckIfMusicIsStillPaused()
         {
+            bool notplaying = false;
+
             ThreadPool.QueueUserWorkItem(method =>
             {
                 Thread.Sleep(5000); //Wait 5 seconds.
-                bool notplaying = false;
                 if (currentActivityState == ActivityStates.Resumed)
-                    if (Build.VERSION.SdkInt > BuildVersionCodes.KitkatWatch)
-                        if (MusicController.MusicStatus != PlaybackStateCode.Playing )
-                        {
-                            notplaying = true;
-                        }
-                        else if(MusicControllerKitkat.MusicStatus!= RemoteControlPlayState.Playing)
-                        {
-                            notplaying = true;
-                        }
-
-                if (notplaying)
                 {
-                    RunOnUiThread(() =>
+                    if (Build.VERSION.SdkInt > BuildVersionCodes.KitkatWatch)
                     {
-                        StopMusicController();
-                        StopFloatingNotificationService();
-                    using (ConfigurationManager configurationManager = new ConfigurationManager(PreferenceManager.GetDefaultSharedPreferences(Application.Context)))                   
-                        using (var wallpaper = WallpaperManager.GetInstance(Application.Context).Drawable)
+                        if (MusicController.MusicStatus != PlaybackStateCode.Playing)
                         {
-                            int savedblurlevel = configurationManager.RetrieveAValue(ConfigurationParameters.BlurLevel, 1);
-                            int savedOpacitylevel = configurationManager.RetrieveAValue(ConfigurationParameters.OpacityLevel, 255);
-                            WallpaperPublisher.ChangeWallpaper(new WallpaperChangedEventArgs { Wallpaper = (BitmapDrawable)wallpaper, OpacityLevel = (short)savedOpacitylevel, BlurLevel = (short)savedblurlevel });
+                            notplaying = true;
                         }
+                    }
+                    else if (MusicControllerKitkat.MusicStatus != RemoteControlPlayState.Playing)
+                    {
+                        notplaying = true;
+                    }
 
-                        Log.Info("LiveDisplay", "Musicfragment stopped");
-                    });
+                    if (notplaying)
+                    {
+                        RunOnUiThread(() =>
+                        {
+                            StopMusicController();
+                            StopFloatingNotificationService();
+                            using (ConfigurationManager configurationManager = new ConfigurationManager(PreferenceManager.GetDefaultSharedPreferences(Application.Context)))
+                            using (var wallpaper = WallpaperManager.GetInstance(Application.Context).Drawable)
+                            {
+                                int savedblurlevel = configurationManager.RetrieveAValue(ConfigurationParameters.BlurLevel, 1);
+                                int savedOpacitylevel = configurationManager.RetrieveAValue(ConfigurationParameters.OpacityLevel, 255);
+                                WallpaperPublisher.ChangeWallpaper(new WallpaperChangedEventArgs { Wallpaper = (BitmapDrawable)wallpaper, OpacityLevel = (short)savedOpacitylevel, BlurLevel = (short)savedblurlevel });
+                            }
 
+                        });
+
+                    }
                 }
             });
         }
@@ -313,12 +323,11 @@
         protected override void OnResume()
         {
             base.OnResume();
-            currentActivityState = ActivityStates.Resumed;
             AddFlags();
             watchDog.Elapsed += WatchdogInterval_Elapsed;
             watchDog.Stop();
             watchDog.Start();
-            OnActivityStateChanged?.Invoke(this, new LockScreenLifecycleEventArgs { State = ActivityStates.Resumed });
+            OnActivityStateChanged?.Invoke(null, new LockScreenLifecycleEventArgs { State = ActivityStates.Resumed });
             if (Build.VERSION.SdkInt > BuildVersionCodes.KitkatWatch)
             {
                 MusicController.MusicPlaying += MusicController_MusicPlaying;
@@ -352,10 +361,9 @@
         protected override void OnPause()
         {
             base.OnPause();
-            currentActivityState = ActivityStates.Paused;
             watchDog.Stop();
             watchDog.Elapsed -= WatchdogInterval_Elapsed;
-            OnActivityStateChanged?.Invoke(this, new LockScreenLifecycleEventArgs { State = ActivityStates.Paused });
+            OnActivityStateChanged?.Invoke(null, new LockScreenLifecycleEventArgs { State = ActivityStates.Paused });
             if (Build.VERSION.SdkInt > BuildVersionCodes.KitkatWatch)
             {
 
@@ -373,10 +381,10 @@
         protected override void OnDestroy()
         {
             base.OnDestroy();
-            currentActivityState = ActivityStates.Destroyed;
-            OnActivityStateChanged?.Invoke(this, new LockScreenLifecycleEventArgs { State = ActivityStates.Destroyed });
-
+            OnActivityStateChanged?.Invoke(null, new LockScreenLifecycleEventArgs { State = ActivityStates.Destroyed });
             //Unbind events
+
+            OnActivityStateChanged -= LockScreenActivity_OnActivityStateChanged;
             //unlocker.Touch -= Unlocker_Touch;
             clearAll.Click -= BtnClearAll_Click;
             WallpaperPublisher.NewWallpaperIssued -= Wallpaper_NewWallpaperIssued;

LiveDisplay/Servicios/FloatingNotification/FloatingNotification.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using LiveDisplay.Activities;
 using LiveDisplay.Adapters;
 using LiveDisplay.Servicios.Notificaciones;
 using LiveDisplay.Servicios.Notificaciones.NotificationEventArgs;
@@ -25,6 +26,7 @@ namespace LiveDisplay.Servicios.FloatingNotification
         private TextView floatingNotificationWhen;
         private LinearLayout floatingNotificationActionsContainer;
         private int position; //Represents the notification position in the NotificationList.
+        private ActivityStates currentActivityState;
 
         public override IBinder OnBind(Intent intent)
         {
@@ -90,23 +92,27 @@ namespace LiveDisplay.Servicios.FloatingNotification
         {
             switch (e.State)
             {
-                case Activities.ActivityStates.Paused:
+                case ActivityStates.Paused:
                     if (floatingNotificationView.Visibility == ViewStates.Visible)
                         floatingNotificationView.Visibility = ViewStates.Invisible;
+
                     break;
 
-                case Activities.ActivityStates.Resumed:
+                case ActivityStates.Resumed:
                     //?
                     break;
 
-                case Activities.ActivityStates.Destroyed:
+                case ActivityStates.Destroyed:
                     if (floatingNotificationView.Visibility == ViewStates.Visible)
                         floatingNotificationView.Visibility = ViewStates.Invisible;
+
                     break;
 
                 default:
                     break;
             }
+            currentActivityState = e.State;
+
         }
 
         private void CatcherHelper_NotificationPosted(object sender, NotificationPostedEventArgs e)
@@ -115,6 +121,7 @@ namespace LiveDisplay.Servicios.FloatingNotification
             {
                 Awake.WakeUpScreen();
             }
+            if(currentActivityState== ActivityStates.Resumed)
             floatingNotificationView.Visibility = ViewStates.Visible;
         }
 