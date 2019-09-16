09670563c8b53483ef10a32520a5d1ca0b3626a5
Carlos Cepeda <carlosalt5126@hotmail.es>
2019-09-16 18:02:32 -0500

    Adding a new entity to WallpaperPoster enum Added 'Weather' entity

--- LiveDisplay/Servicios/Wallpaper/WallpaperPosters.cs
+++ LiveDisplay/Servicios/Wallpaper/WallpaperPosters.cs
@@ -20,5 +20,10 @@ namespace LiveDisplay.Servicios.Wallpaper
         MusicPlayer = 2,
         Notification = 3, //A notification can also set a wallpaper, 
         //for example, the MediaStyle and the BigPictureStyle of a notification uses the Big Picture to use it as the wallpaper.
+        Weather= 4 //The weather also posts wallpapers, depending on a certain weather condition the lockscreen can change it's wallpaper 
+            //when the Weather is clicked on the lockscreen, for example, if the current weather is sunny, the wallpaper can change to a image of a sun and clear sky.
+            //This premise may not happen at all but it's my first idea.
+            //(TODO) actually this is not happening at all, ha ha.
+
     }
 }
