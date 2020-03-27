41a3839de2ae06eeea258c13e76e5a8d1bf7a7e8
Carlos Cepeda <carlosalt5126@hotmail.es>
2020-03-27 12:59:56 -0500

    Avoid downloading information when Awake is sleeping.

--- LiveDisplay/Servicios/Weather/GrabWeatherJob.cs
+++ LiveDisplay/Servicios/Weather/GrabWeatherJob.cs
@@ -1,22 +1,21 @@
-﻿using Android.App;
-using Android.App.Job;
-using Android.Content;
+﻿using Android.Content;
 using Android.Util;
 using AndroidX.Work;
 using LiveDisplay.Misc;
-using System.Threading.Tasks;
 
 namespace LiveDisplay.Servicios.Weather
 {
     public class GrabWeatherJob : Worker
     {
-        bool success = false;
         public GrabWeatherJob(Context context, WorkerParameters workerParameters) : base(context, workerParameters)
         {
 
         }
         public override Result DoWork()
         {
+            if (AwakeService.GetAwakeStatus() == Awake.AwakeStatus.Sleeping)
+                return Result.InvokeSuccess(); //We want to keep the job running but don't do the job itself while Awake is sleeping.
+
             ConfigurationManager configurationManager = new ConfigurationManager(AppPreferences.Weather);
 
             string city = configurationManager.RetrieveAValue(ConfigurationParameters.WeatherCity, "New York");
