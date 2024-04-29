cfc6345fc537d804cba07c942001f7c64e874d34
Carlos Cepeda <carlosalt5126@hotmail.es>
2024-04-29 17:12:16 -0500

    Improving readaility of WallpaperPublisher moving the timeout functionality to a separate method

--- LiveDisplay/Servicios/Wallpaper/WallpaperPublisher.cs
+++ LiveDisplay/Servicios/Wallpaper/WallpaperPublisher.cs
@@ -1,12 +1,9 @@
-﻿using Android.App;
-using Android.Content;
-using Android.Graphics.Drawables;
-using Android.Util;
+﻿using Android.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Contexts;
-using System.Threading;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace LiveDisplay.Servicios.Wallpaper
 {
@@ -14,11 +11,13 @@ namespace LiveDisplay.Servicios.Wallpaper
     {
         private const int infiniteSeconds = 0;
 
-        private static List<WallpaperChangedEventArgs> wallpaperPosters = new List<WallpaperChangedEventArgs>();
+        private static readonly List<WallpaperChangedEventArgs> wallpaperPosters = new List<WallpaperChangedEventArgs>();
 
         public static event EventHandler<WallpaperChangedEventArgs> NewWallpaperIssued; //This event notifies listeners the Wallpaper that has been issued.
 
                                                                                         //For now the only listener of this event is 'LockScreen'
+        private static Timer timeoutTimer;
+        private static WallpaperChangedEventArgs temporalWallpaperPoster;
 
         public static void ChangeWallpaper(WallpaperChangedEventArgs e)
         {
@@ -26,26 +25,14 @@ namespace LiveDisplay.Servicios.Wallpaper
             {
                 wallpaperPosters.Add(e);
             }
-            Console.WriteLine($"{e.WallpaperPoster} is posting a {(e.SecondsOfAttention > infiniteSeconds ? "Temporal" : "Permanent")} wallpaper, duration {e.SecondsOfAttention * 1000} seconds");
+            Console.WriteLine($"{e.WallpaperPoster} is posting a {(e.SecondsOfAttention > infiniteSeconds ? "Temporal" : "Permanent")} wallpaper, duration {e.SecondsOfAttention} seconds");
 
             if (e.SecondsOfAttention>infiniteSeconds)
             {
-                var temporalWallpaperPoster = e;
+                temporalWallpaperPoster = e;
 
-                using (var timeoutTimer = new System.Timers.Timer{ Interval= e.SecondsOfAttention * 1000, AutoReset= false})
-                {
-                    timeoutTimer.Start();
-                    timeoutTimer.Elapsed += (sender, e) => 
-                    {
-                        Console.WriteLine($"{temporalWallpaperPoster.WallpaperPoster} is being removed because it was temporary, duration {temporalWallpaperPoster.SecondsOfAttention} seconds");
-                        wallpaperPosters.Remove(temporalWallpaperPoster);
-                        if (wallpaperPosters.Count >= 1)
-                        {
-                            Console.WriteLine($"posting previous wallpaper from {wallpaperPosters[^1].WallpaperPoster}");
-                            NewWallpaperIssued?.Invoke(null, wallpaperPosters[^1]); //after removing this temporal wallpaper, issue the previous wallpaper, if available.
-                        }
-                    };
-                }
+                StartTimeout(e.SecondsOfAttention * 1000);
+               
             }
 
 
@@ -73,5 +60,27 @@ namespace LiveDisplay.Servicios.Wallpaper
             NewWallpaperIssued?.Invoke(null, e);
         }
 
+        private static void StartTimeout(int interval)
+        {
+            timeoutTimer = new Timer
+            {
+                Interval = interval,
+                AutoReset = false
+            };
+            timeoutTimer.Elapsed += TimeoutTimer_Elapsed;
+            timeoutTimer.Start();
+        }
+
+        private static void TimeoutTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine($"{temporalWallpaperPoster.WallpaperPoster} is being removed because it was temporary, duration {temporalWallpaperPoster.SecondsOfAttention} seconds");
+            wallpaperPosters.Remove(temporalWallpaperPoster);
+            if (wallpaperPosters.Count >= 1)
+            {
+                Console.WriteLine($"posting previous wallpaper from {wallpaperPosters[^1].WallpaperPoster}");
+                NewWallpaperIssued?.Invoke(null, wallpaperPosters[^1]); //after removing this temporal wallpaper, issue the previous wallpaper, if available.
+            }
+            timeoutTimer.Stop();
+        }
     }
 }
