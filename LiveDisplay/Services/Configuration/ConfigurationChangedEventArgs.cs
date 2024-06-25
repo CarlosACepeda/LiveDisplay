41ce8600b16eedc6cebe5f7ab0debef63c47b209
Carlos Cepeda <carlosalt5126@hotmail.es>
2024-06-24 19:42:22 -0500

    Created a SharedPreference listener service, this service exposes an Event which allows listeners to be informed about any change in any shared preference.

---
+++ LiveDisplay/Services/Configuration/ConfigurationChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LiveDisplay.Services.Configuration 
+{
+    public class ConfigurationChangedEventArgs : EventArgs
+    {
+        public string Key { get; set; }
+
+        public object Value { get; set; }
+        public Type ValueType { get; set; }
+
+    }
+
+}
---
+++ LiveDisplay/Services/SharedPreferencesListenerService.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.Preference;
+using LiveDisplay.Services.Configuration;
+using System;
+
+namespace LiveDisplay.Services
+{
+    [Service(Label = "@string/app_name")]
+    public class SharedPreferenceListenerService : Service, ISharedPreferencesOnSharedPreferenceChangeListener
+    {
+        private readonly ISharedPreferences sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(Application.Context);
+        public static event EventHandler<ConfigurationChangedEventArgs> ConfigurationChanged;
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public void OnSharedPreferenceChanged(ISharedPreferences sharedPreferences, string key)
+        {
+            Type type= null;
+            object value= null;
+            foreach(var sharedPreference in sharedPreferences.All)
+            {
+                if (sharedPreference.Key == key)
+                {
+                    value = sharedPreference.Value;
+                    type = sharedPreference.Value.GetType();
+                }
+            }
+
+            OnConfigurationChanged(new ConfigurationChangedEventArgs
+            {
+                Key = key,
+                Value = value,
+                ValueType = type
+            });
+
+            Console.WriteLine($"{key}: was CHANGED");
+        }
+
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            Console.WriteLine("SHARED PREFERENCE LISTENER ACTIVE!");
+            sharedPreferences.RegisterOnSharedPreferenceChangeListener(this);
+
+            return base.OnStartCommand(intent, flags, startId);
+        }
+        public override void OnDestroy()
+        {
+            sharedPreferences.UnregisterOnSharedPreferenceChangeListener(this);
+            Console.WriteLine("SHARED PREFERENCE LISTENER NOT ACTIVE!");
+            base.OnDestroy();
+        }
+        void OnConfigurationChanged(ConfigurationChangedEventArgs e)
+        {
+            ConfigurationChanged?.Invoke(
+                null, e);
+        }
+    }
+}
