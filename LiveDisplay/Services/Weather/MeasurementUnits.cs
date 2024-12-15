3b1d94d99cf0c66becee77724f20e59c702c940a
Carlos Cepeda <carlosalt5126@hotmail.es>
2024-12-15 15:22:58 -0500

    Adding string constants for measurement unit representation when saving weather info

--- LiveDisplay/Services/Weather/MeasurementUnits.cs
+++ LiveDisplay/Services/Weather/MeasurementUnits.cs
@@ -1,9 +1,15 @@
-﻿namespace LiveDisplay.Services.Weather
+﻿using Android.App;
+using LiveDisplay.Misc;
+
+namespace LiveDisplay.Services.Weather
 {
     public class MeasurementUnits
-    {
+    { 
         public const string Kelvin = "";
         public const string Celsius = "metric";
         public const string Fahrenheit = "imperial";
+        public const string KelvinRepresentation = "°K";
+        public const string CelsiusRepresentation = "°C";
+        public const string FahrenheitRepresentation = "°F";
     }
 }
