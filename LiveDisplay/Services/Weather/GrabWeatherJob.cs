203c8927dd5b9652802b67f9ac0193e594d5af00
Carlos Cepeda <carlosalt5126@hotmail.es>
2024-12-15 15:30:18 -0500

    Set a string constant so we can use it as a Tag later when interacting with this job

--- LiveDisplay/Services/Weather/GrabWeatherJob.cs
+++ LiveDisplay/Services/Weather/GrabWeatherJob.cs
@@ -1,18 +1,17 @@
 ﻿using Android.App;
 using Android.Content;
 using Android.Locations;
-using Android.Util;
+using Android.Nfc;
 using AndroidX.Work;
 using Java.Util;
 using LiveDisplay.Misc;
-using LiveDisplay.Services.Awake;
 using System;
-using System.Threading;
 
 namespace LiveDisplay.Services.Weather
 {
     public class GrabWeatherJob : Worker
     {
+        public const string Tag = "GrabWeatherJob";
         public GrabWeatherJob(Context context, WorkerParameters workerParameters) : base(context, workerParameters)
         {
 
@@ -23,7 +22,6 @@ namespace LiveDisplay.Services.Weather
             {
                 LocationManager locationManager = (LocationManager)Application.Context.GetSystemService(Context.LocationService);
                 var loc = locationManager.GetLastKnownLocation(LocationManager.GpsProvider);
-                Console.WriteLine($"LOCATION IS {loc?.Latitude},{loc?.Longitude}");
 
                 var result = OpenWeatherMapClient.GetWeather(loc.Latitude.ToString(), loc.Longitude.ToString(), MeasurementUnits.Celsius, Locale.Default.Language)?.Result;
                 if (result != null)
